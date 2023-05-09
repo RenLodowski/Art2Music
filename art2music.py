8723a534074f08a6fffce62d920e23223affa440
RenLodowski <lodowsra01@sunyjcc.edu>
2023-05-09 19:49:57 -0400

    Update art2music.py

--- art2music.py
+++ art2music.py
@@ -353,7 +353,7 @@ if img2load is not None:
         img = img.convert('RGB')
 
 # Save the image as JPEG or PNG based on the file extension
-    if img2load.endswith('.png'):
+    if img2load.format == 'PNG':
     # If it's a PNG, save it with no alpha channel
         img.save('img.jpg', quality=95)
     else:
